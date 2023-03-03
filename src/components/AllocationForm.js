b55a2e485cb7e396a8d9dc1d02076d8644bb7db4
Anna-logv <an.logvinenko@gmail.com>
2023-03-03 16:36:35 -0500

    fix
    Signed-off-by: Anna-logv <an.logvinenko@gmail.com>

--- src/components/AllocationForm.js
+++ src/components/AllocationForm.js
@@ -5,28 +5,28 @@ const AllocationForm = (props) => {
     const [name, setName] = useState('');
     const [cost, setCost] = useState('');
     const [action, setAction] = useState('');
-    const submitEvent = () => {
-        if(cost > remaining) {
-            alert("The value cannot exceed remaining funds  £"+remaining);
-            setCost("");
-            return;
-        }
-    const expense = {
-        name: name,
-        cost: parseInt(cost),
-    };
-    if(action === "Reduce") {
-        dispatch({
-            type: 'RED_EXPENSE',
-            payload: expense,
-        });
-    } else {
+  const submitEvent = () => {
+            if(cost > remaining) {
+                alert("The value cannot exceed remaining funds  £"+remaining);
+                setCost("");
+                return;
+            }
+        const expense = {
+            name: name,
+            cost: parseInt(cost),
+        };
+        if(action === "Reduce") {
             dispatch({
-                type: 'ADD_EXPENSE',
+                type: 'RED_EXPENSE',
                 payload: expense,
             });
-        }
-};
+        } else {
+                dispatch({
+                    type: 'ADD_EXPENSE',
+                    payload: expense,
+                });
+            }
+    };
     return (
         <div>
             <div className='row'>
