6efde0101684fe2e0bd5f02d7e946c85d5fb2386
Your Name <you@example.com>
2023-03-03 16:14:19 -0500

    commit all
    Signed-off-by: Your Name <you@example.com>

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,1 +1,30 @@
-
+import React, {useContext} from 'react';
+import {TiDelete} from 'react-icons/ti';
+import {AppContext} from '../context/AppContext';
+const ExpenseItem=(props)=>{
+    const {dispatch}=useContext(AppContext);
+    const handleDel=()=>{
+        dispatch({
+            type:'DELETE_EXPENSE',
+            payload: props.id,
+        })
+    }
+    const incAllocation=(name)=>{
+        dispatch({
+            type:'ADD_EXPENSE',
+            payload: {
+                name:name,
+                cost:10,
+            },
+        })
+    }
+    return (
+        <tr>
+        <td>{props.name}</td>
+        <td>${props.cost}</td>   
+        <td><button onClick={e=>incAllocation(props.name)}>+</button></td>
+        <td><TiDelete size='1.5em' onClick={handleDel}></TiDelete></td>
+        </tr>
+    )
+}
+export default ExpenseItem;
